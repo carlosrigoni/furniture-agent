56d69f9b7d93a8524e5f26e1dd372d6dd3b48c92
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-04-13 18:07:02 -0300

    feat: Interface NewProject

--- src/pages/NewProject/index.js
+++ src/pages/NewProject/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, Title, Form, Input, Button, TextSubmit, InputHeader } from './styles';
 
 export default function NewProject() {
   return (
-    <View />
+    <Container>
+        <Title>Criar novo projeto</Title>
+      <Form>
+
+        <InputHeader>
+          <Input
+            autoCorrect={false}
+            placeholder="Nome do Projeto"
+          />
+          <Input
+            autoCorrect={false}
+            placeholder="Nome do Cliente"
+          />
+        </InputHeader>
+
+        <Input
+          autoCorrect={false}
+          placeholder="Endereco"
+        />
+
+        <Button>
+          <TextSubmit>Cadastrar</TextSubmit>
+        </Button>
+
+      </Form>
+    </Container>
   );
 }
---
+++ src/pages/NewProject/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient'
+import {getStatusBarHeight } from 'react-native-status-bar-height'
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#10bac9'],
+  start: { x: 0, y: 0 },
+  end: { x: 1, y: 1 },
+})`
+  flex: 1;
+  padding-top: ${30 + getStatusBarHeight(true)}
+`;
+
+export const Title = styled.Text`
+  margin-top: 160px;
+  text-align: center;
+  font-size: 32px;
+  color: #FFF;
+  font-weight: bold;
+  padding: 0 20px;
+`;
+
+export const Form = styled.View`
+  margin-top: 10px;
+  background-color: #fff;
+  margin-left: 15px;
+  margin-right: 15px;
+  border-radius: 4px;
+  padding: 20px;
+
+
+
+`;
+
+export const InputHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 14px;
+
+`;
+
+
+export const Button = styled.TouchableOpacity`
+  background-color: #3870FF;
+  width: 320px;
+  height: 42px;
+  margin-top: 20px;
+  border-radius: 6px;
+
+`;
+
+export const TextSubmit = styled.Text`
+  text-align: center;
+  top: 7px;
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+`;
