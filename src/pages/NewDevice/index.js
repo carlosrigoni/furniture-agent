a5836d4b4bb8533c70dfceb082b6f6845cb76e91
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-04-14 16:23:07 -0300

    feat: Interface NewDeviceScreens

--- src/pages/NewDevice/index.js
+++ src/pages/NewDevice/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, Title, Form, Input, Button, TextSubmit, InputHeader } from './styles';
 
-export default function NewDevice() {
+export default function NewProject() {
   return (
-    <View />
+    <Container>
+        <Title>Adicionar dispositivo</Title>
+      <Form>
+
+        <InputHeader>
+          <Input
+            autoCorrect={false}
+            placeholder="Dispositivo ID:"
+          />
+          <Input
+            autoCorrect={false}
+            placeholder="Nome do dispositivo:"
+          />
+        </InputHeader>
+
+        <Input
+          autoCorrect={false}
+          placeholder="Fabricante:"
+        />
+
+        <Button>
+          <TextSubmit>Adicionar</TextSubmit>
+        </Button>
+
+      </Form>
+    </Container>
   );
 }
--- src/pages/NewDeviceScenario/index.js
+++ src/pages/NewDeviceScenario/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import {Picker} from '@react-native-community/picker';
 
-// import { Container } from './styles';
 
-export default function NewDeviceScenario() {
+import { Container, Title, Form, Input, Button, TextSubmit, InputHeader } from './styles';
+
+export default function NewProject() {
+  const [type, setType] = useState('Tipo')
+
+
   return (
-    <View />
+    <Container>
+        <Title>Adicionar dispositivo Scenario</Title>
+      <Form>
+
+        <InputHeader>
+          <Input
+            autoCorrect={false}
+            placeholder="Dispositivo ID:"
+          />
+          <Input
+            autoCorrect={false}
+            placeholder="Nome do dispositivo:"
+          />
+        </InputHeader>
+
+        <Picker
+          selectedValue={type}
+          style={{height: 50, width: 230}}
+          onValueChange={(itemValue, itemIndex) =>
+            setType(itemValue)
+          }>
+          <Picker.Item label="ModuloIluminacao" value="ModuloIluminacao" />
+          <Picker.Item label="Teclados" value="Teclados" />
+          <Picker.Item label="ModuloControleCortina" value="ModuloControleCortina" />
+        </Picker>
+
+        <Button>
+          <TextSubmit>Adicionar</TextSubmit>
+        </Button>
+
+      </Form>
+    </Container>
   );
 }
