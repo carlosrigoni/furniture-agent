a842439c00e5c0b067691380abdc8137f13fada2
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-04-14 16:22:00 -0300

    feat: Interface AmbienceScreen

--- src/pages/AmbienceScreen/index.js
+++ src/pages/AmbienceScreen/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
+import defaultRoomImg from '../../assets/default-room.jpg'
+import Icon from 'react-native-vector-icons/Entypo'
+import { useNavigation } from '@react-navigation/native'
 
-// import { Container } from './styles';
+import { Container, Header, Room, Title, ButtonsAdd, ButtonAdd, TextButton, List, DeviceContainer, DeviceText, DeviceId, TrashButton } from './styles';
 
 export default function AmbienceScreen() {
+  const navigation = useNavigation()
+
+
+  function navigateToNewDevice() {
+    navigation.navigate('NewDevice')
+  }
+
+  function navigateToNewDeviceScenario() {
+    navigation.navigate('NewDeviceScenario')
+  }
+
+
   return (
-    <View />
+    <Container>
+      <Header>
+      <Room source={defaultRoomImg} />
+      <Title>SALA DE ESTAR</Title>
+      </Header>
+
+      <ButtonsAdd>
+        <ButtonAdd onPress={navigateToNewDeviceScenario}>
+          <TextButton>ADD SCENARIO</TextButton>
+        </ButtonAdd>
+
+        <ButtonAdd onPress={navigateToNewDevice}>
+          <TextButton>ADD OUTRO</TextButton>
+        </ButtonAdd>
+      </ButtonsAdd>
+
+      <List keyboardShouldPersistTaps="handled"
+      data={[1,2, 3, 4, 5, 6, ,7, 8, 9, 10, 11, 12, 18]}
+      keyExtractor={item => String(item)}
+      renderItem={({ item }) =>  (
+        <DeviceContainer>
+          <DeviceText>Google Alexa -  FAB GOOGLE</DeviceText>
+          <DeviceId>id: 46473</DeviceId>
+          <TrashButton>
+            <Icon name="500px" size={22} color="#fff" />
+          </TrashButton>
+        </DeviceContainer>
+
+      )}/>
+
+
+    </Container>
   );
 }
---
+++ src/pages/AmbienceScreen/styles.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient'
+import {getStatusBarHeight } from 'react-native-status-bar-height'
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#10bac9'],
+  start: { x: 0, y: 0 },
+  end: { x: 1, y: 1 },
+})`
+  flex: 1;
+  padding-top: ${getStatusBarHeight(true)}px
+`;
+
+
+export const Header = styled.View`
+  background-color: #317CEC;
+`;
+
+export const Room = styled.Image`
+  width: 100%;
+  height: 250px;
+`;
+
+export const Title = styled.Text`
+  font-family: Roboto;
+  font-weight: bold;
+  font-size: 23px;
+  line-height: 25px;
+  text-align: center;
+  color: #fff;
+  padding: 15px;
+
+`;
+
+export const ButtonsAdd = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const ButtonAdd = styled.TouchableOpacity`
+  background-color: #fff;
+  padding: 13px;
+  width: 40%;
+  border-radius: 10px;
+`;
+
+export const TextButton = styled.Text`
+  font-weight: bold;
+  color: #317CEC;
+  text-align: center;
+`;
+
+export const List = styled.FlatList.attrs({
+  contentContainerStyle: { paddingHorizontal: 20 },
+  showsVerticalScrollIndicator: false,
+})`
+
+`;
+
+export const DeviceContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 3px;
+  margin-bottom: 15px;
+`;
+
+export const DeviceText = styled.Text`
+  color: #fff;
+`;
+
+export const DeviceId = styled.Text`
+  color: #fff;
+
+`;
+
+export const TrashButton = styled.TouchableOpacity`
+  bottom: 6px;
+
+`;
+
