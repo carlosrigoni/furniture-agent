99965150c1e709f98187271091f96dc99e07d054
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-04-14 10:37:12 -0300

    feat: interface ProjectScreen

--- src/pages/ProjectScreen/index.js
+++ src/pages/ProjectScreen/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo'
+import { useNavigation } from '@react-navigation/native'
+import defaultRoomImg from '../../assets/default-room.jpg'
 
-// import { Container } from './styles';
+import { Container, Title, Header,ProjectContainer, ListHeader, ProjectText, Date, List, TitleAmbience, Room, RoomSpace} from './styles';
 
 export default function ProjectScreen() {
+  const navigation = useNavigation()
+
+  function navigateToHome() {
+    navigation.navigate('HomeScreen')
+  }
+
+  function navigateToNewAmbience() {
+    navigation.navigate('NewAmbience')
+  }
+
+  function navigateToAmbience() {
+    navigation.navigate('Ambience')
+  }
+
   return (
-    <View />
+    <Container>
+      <Header >
+        <TouchableOpacity onPress={navigateToHome}>
+          <Icon name="500px" size={22} color="#000" />
+        </TouchableOpacity>
+
+        <Title>Scenario</Title>
+
+        <TouchableOpacity onPress={navigateToNewAmbience}>
+          <Icon name="500px" size={22} color="#000" />
+        </TouchableOpacity>
+      </Header>
+
+      <ProjectContainer>
+        <ListHeader>
+        <ProjectText>Novo apartamento</ProjectText>
+        <Date>02/11/2020</Date>
+        </ListHeader>
+        <ProjectText>Carlos Daniel Rigoni</ProjectText>
+        <ProjectText>Rua Joao Marciano</ProjectText>
+      </ProjectContainer>
+
+      <List
+      keyboardShouldPersistTaps="handled"
+      data={[1,2, 3]}
+      keyExtractor={item => String(item)}
+      renderItem={({ item }) =>  (
+        <RoomSpace>
+          <TitleAmbience>Sala de estar</TitleAmbience>
+          <Room source={defaultRoomImg} />
+        </RoomSpace>
+
+
+      )}/>
+
+    </Container>
   );
 }
---
+++ src/pages/ProjectScreen/styles.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient'
+import {getStatusBarHeight } from 'react-native-status-bar-height'
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#10bac9'],
+  start: { x: 0, y: 0 },
+  end: { x: 1, y: 1 },
+})`
+  flex: 1;
+  padding-top: ${30 + getStatusBarHeight(true)}
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  color: #FFF;
+  font-weight: bold;
+  padding: 0 20px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+
+`;
+
+export const ProjectContainer = styled.View`
+  margin-top: 15px;
+  background-color: #fff;
+  margin-left: 19px;
+  margin-right: 19px;
+  border-radius: 4px;
+  padding: 20px;
+
+`;
+
+export const ListHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ProjectText = styled.Text`
+  color: #666;
+  font-weight: bold;
+  margin-top: 5px;
+  line-height: 20px;
+`;
+
+export const Date = styled.Text`
+  font-size: 12px;
+  color: #666;
+  text-align: right;
+
+`;
+
+export const List = styled.FlatList.attrs({
+  contentContainerStyle: { paddingHorizontal: 20 },
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 10px;
+
+`;
+
+export const RoomSpace = styled.TouchableOpacity`
+  margin-top: 10px;
+  margin-bottom: 15px;
+  border-radius: 4px;
+  width: 100%;
+  height: 240px;
+`;
+
+export const TitleAmbience = styled.Text`
+  margin-top: 5px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 15px;
+
+
+`;
+
+export const Room = styled.Image`
+margin-top: 3px;
+  width: 90%;
+  height: 200px;
+  left: 15px;
+  border-radius: 4px;
+`;
+
