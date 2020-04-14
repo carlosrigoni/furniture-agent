054cd9f4c570652bbeec477579d04cb7f9e25b2b
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-04-14 11:53:16 -0300

    feat: Interface NewAmbience

--- src/pages/NewAmbience/index.js
+++ src/pages/NewAmbience/index.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import defaultRoomImg from '../../assets/default-room.jpg'
 
-// import { Container } from './styles';
+import { Container, Title, Form, Input, Button, TextSubmit, ButtonGetImage, Room, TextImage } from './styles';
 
-export default function NewAmbience() {
+export default function NewProject() {
+  const options = {
+    title: 'Selecione uma foto do ambiente',
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+  function getImage() {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else {
+        const source = { uri: response.uri };
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        this.setState({
+          avatarSource: source,
+        });
+      }
+    })
+  }
+
   return (
-    <View />
+    <Container>
+        <Title>Criar novo ambiente</Title>
+      <Form>
+
+        <Input
+          autoCorrect={false}
+          placeholder="Nome do ambiente"
+        />
+
+        <ButtonGetImage onPress={getImage}>
+          <Room source={defaultRoomImg} />
+          <TextImage>Abrir Camera</TextImage>
+        </ButtonGetImage>
+        <Button>
+          <TextSubmit>Cadastrar</TextSubmit>
+        </Button>
+
+      </Form>
+    </Container>
   );
 }
---
+++ src/pages/NewAmbience/styles.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient'
+import {getStatusBarHeight } from 'react-native-status-bar-height'
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#10bac9'],
+  start: { x: 0, y: 0 },
+  end: { x: 1, y: 1 },
+})`
+  flex: 1;
+  padding-top: ${30 + getStatusBarHeight(true)}px
+`;
+
+export const Title = styled.Text`
+  margin-top: 100px;
+  text-align: center;
+  font-size: 32px;
+  color: #FFF;
+  font-weight: bold;
+  padding: 0 20px;
+`;
+
+export const Form = styled.View`
+  margin-top: 10px;
+  background-color: #fff;
+  margin-left: 15px;
+  margin-right: 15px;
+  border-radius: 4px;
+  padding: 20px;dfkfs
+
+
+
+`;
+
+export const InputHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 14px;
+
+`;
+
+
+export const Button = styled.TouchableOpacity`
+  background-color: #3870FF;
+  width: 100%;
+  height: 42px;
+  margin-top: 20px;
+  border-radius: 6px;
+
+`;
+
+export const TextSubmit = styled.Text`
+  text-align: center;
+  top: 7px;
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const TextImage = styled.Text`
+  text-align: center;
+  font-size: 15px;
+  color: #444;
+  font-weight: bold;
+`;
+
+export const ButtonGetImage = styled.TouchableOpacity`
+  border-radius: 6px;
+
+`;
+
+export const Room = styled.Image`
+  width: 90%;
+  height: 200px;
+  left: 15px;
+  border-radius: 4px;
+`;
+
